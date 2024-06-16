f8de5e3d9d593523321bd7b86e3888d5d98a4138
Well35 <52145150+Well35@users.noreply.github.com>
2024-06-16 14:58:51 -0700

    Add OR gate

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -2,14 +2,12 @@ extends CanvasLayer
 
 var input_pin_scene = preload("res://scenes/input_pin.tscn")
 var and_gate_scene = preload("res://scenes/and_gate.tscn")
-#var or_gate_scene = preload("res://scenes/or_gate.tscn")
+var or_gate_scene = preload("res://scenes/or_gate.tscn")
 
 signal eval
 
 func _on_and_gate_button_pressed():
-	var and_gate = and_gate_scene.instantiate()
-	and_gate.position = Vector2(get_viewport().size.x / 2, get_viewport().size.y / 2)
-	get_parent().get_child(1).add_child(and_gate)
+	create_gate(and_gate_scene)
 
 
 func _on_input_node_button_pressed():
@@ -18,6 +16,14 @@ func _on_input_node_button_pressed():
 	get_parent().get_child(2).add_child(input_pin)
 	Globals.input_pins += 1
 
+func create_gate(gate_scene):
+	var gate = gate_scene.instantiate()
+	gate.position = Vector2(get_viewport().size.x / 2, get_viewport().size.y / 2)
+	get_parent().get_child(1).add_child(gate)
 
 func _on_evaluate_button_pressed():
 	eval.emit()
+
+
+func _on_or_gate_button_pressed():
+	create_gate(or_gate_scene)
