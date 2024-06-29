f84012fcf4040b96ddd6d7e839e858a330a66eb5
Well35 <52145150+Well35@users.noreply.github.com>
2024-06-29 16:10:15 -0700

    Fix wire placement when attaching to buttons

--- scripts/wire.gd
+++ scripts/wire.gd
@@ -1,16 +1,17 @@
 extends Line2D
 
-var start_pos: Vector2
-var end_pos: Vector2
 var start_button: Button
 var end_button: Button
 
 @onready var rect: CollisionShape2D = $Area2D/CollisionShape2D
 var shape = RectangleShape2D.new()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
-	set_point_position(0, start_button.global_position + start_button.size / 2)
-	set_point_position(1, end_button.global_position)
+	var start_position: Vector2 = start_button.global_position + start_button.size / 2
+	var end_position: Vector2 = end_button.global_position + end_button.size / 2
+	
+	set_point_position(0, start_position)
+	set_point_position(1, end_position)
 	
 	shape.size.x = get_point_position(1).distance_to(get_point_position(0))
 	shape.size.y = width
@@ -18,7 +19,7 @@ func _process(delta):
 	rect.rotation = (get_point_position(1) - get_point_position(0)).normalized().angle()
 	rect.position = (get_point_position(0) + get_point_position(1)) / 2
 
-# When user right-clicks on a wire, remove it and clear button properties
+
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("remove"):
 		start_button.transfer_button = null
